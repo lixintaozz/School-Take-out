b6b881c40c2fd831c32885f720289d1c9f8daf27
warrior <1986411713@qq.com>
2024-12-08 11:02:16 +0800

    添加Springboot自动配置原理相关知识点

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -6,6 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+/*
+@SpringBootApplication包含的3个关键注解：
+- @SpringBootConfiguration：实际上的作用就是@Configuration
+- @EnableAutoConfiguration：作用为扫描引入的依赖jar包中的配置类，将其放入IOC容器管理
+                            实现原理：通过起步依赖中的两个文件得到所有依赖中所以配置类的完全限定名，然后使用@import来导入IOC容器
+- @ComponentScan(
+    excludeFilters = {@Filter(
+    type = FilterType.CUSTOM,
+    classes = {TypeExcludeFilter.class}
+), @Filter(
+    type = FilterType.CUSTOM,
+    classes = {AutoConfigurationExcludeFilter.class}
+)}
+)：作用为扫描启动类所在包及其子包的配置类
+ */
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 public class SkyApplication {
