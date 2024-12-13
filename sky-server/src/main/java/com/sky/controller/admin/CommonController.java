f1d31b6809a6377a74ad515f2926b9c90eeb7ea7
warrior <1986411713@qq.com>
2024-12-13 11:03:29 +0800

    添加文件上传接口相关代码

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,39 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
+public class CommonController {
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @ApiOperation("文件上传")
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+        assert originalFilename != null;
+        String filename = uuid + originalFilename.substring(originalFilename.lastIndexOf("."));
+        //文件保存在本机磁盘
+        file.transferTo(new File("D:\\SkyFast\\img\\" + filename));
+        //文件的url访问路径
+        String path = "http://localhost:8080/img/" + filename;   //注意：这里的path必须加上协议http://
+        return Result.success(path);
+    }
+}
