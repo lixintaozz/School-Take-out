06741e3c8bd34af034a5a33740042138a45a127c
warrior <1986411713@qq.com>
2024-12-19 11:01:34 +0800

    完成店铺状态更改相关代码

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,40 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController("adminShopController")
+@Api(tags = "店铺相关接口")
+@RequestMapping("/admin/shop")
+public class ShopController {
+    public static final String SHOP_STATUS = "shop_status";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @ApiOperation("获取店铺营业状态")
+    @GetMapping("/status")
+    public Result<Integer> getShopStatus()
+    {
+        log.info("获取店铺营业状态...");
+        Integer status = (Integer) redisTemplate.opsForValue().get(SHOP_STATUS);
+        return Result.success(status);
+    }
+
+    @ApiOperation("设置店铺营业状态")
+    @PutMapping("/{status}")
+    public Result setShopStatus(@PathVariable Integer status)
+    {
+        log.info("设置店铺营业状态为: {}", status == 1 ? "营业中" : "已打烊");
+        redisTemplate.opsForValue().set(SHOP_STATUS, status);
+        return Result.success();
+    }
+}
