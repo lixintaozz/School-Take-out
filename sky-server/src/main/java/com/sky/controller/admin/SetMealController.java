b3a5031f04aeedfc19193653e58e0d9ab10c9b59
warrior <1986411713@qq.com>
2024-12-25 10:38:33 +0800

    添加使用SpringCache缓存菜品相关代码

--- sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -45,6 +46,7 @@ public class SetMealController {
      */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache", key = "#setmealDTO.categoryId")
     public Result insert(@RequestBody SetmealDTO setmealDTO)
     {
         log.info("新增套餐: {}", setmealDTO);
@@ -59,6 +61,7 @@ public class SetMealController {
      */
     @ApiOperation("批量删除套餐")
     @DeleteMapping
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result delete(@RequestParam List<Long> ids)
     {
         log.info("批量删除套餐: {}", ids);
@@ -74,6 +77,7 @@ public class SetMealController {
      */
     @PostMapping("/status/{status}")
     @ApiOperation("起售/停售套餐")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result startOrStop(@PathVariable Integer status, long id)
     {
         log.info("起售/停售套餐: {}", id);
@@ -87,7 +91,7 @@ public class SetMealController {
      * @return
      */
     @GetMapping("/page")
-    Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO)
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO)
     {
         log.info("套餐分页查询: {}", setmealPageQueryDTO);
         PageResult pageResult = setMealService.pageQuery(setmealPageQueryDTO);
@@ -101,7 +105,8 @@ public class SetMealController {
      */
     @ApiOperation("修改套餐")
     @PutMapping
-    Result update(@RequestBody SetmealDTO setmealDTO)
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
+    public Result update(@RequestBody SetmealDTO setmealDTO)
     {
         log.info("修改套餐: {}", setmealDTO);
         setMealService.updateWithDishes(setmealDTO);
